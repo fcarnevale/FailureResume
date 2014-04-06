b44f65c63df7353733fa7c7506b79a031d3ba628
Frank Carnevale <frank@fourward.org>
2014-04-06 19:03:30 -0400

    Annotate models

--- app/models/failure.rb
+++ app/models/failure.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: failures
+#
+#  id          :integer          not null, primary key
+#  resume_id   :integer
+#  description :string(255)
+#  lesson      :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Failure < ActiveRecord::Base
   belongs_to :resume
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id             :integer          not null, primary key
+#  twitter_handle :string(255)
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 class User < ActiveRecord::Base
   has_one :resume
 end
