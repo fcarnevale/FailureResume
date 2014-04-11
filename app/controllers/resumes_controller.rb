3086e7f0356f8ec8690d45d36992ff2bd1a0af7f
Frank Carnevale <frank@fourward.org>
2014-04-10 20:25:13 -0400

    Add some before actions to resumes controller
    *Lots of temporary implementations here (until proper user auth/login
    gets added)

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,8 @@
 class ResumesController < ApplicationController
-  before_action :load_resume
-  
+  before_action :load_user, except: :show
+  before_action :load_new_resume, only: :new
+  before_action :load_resume_and_user, only: :show
+
   def home
     # FIXME: temporary (eventually use login/resume status to redirect appropriately)
     render action: :new
@@ -10,10 +12,22 @@ class ResumesController < ApplicationController
   end
 
   private
-
-    def load_resume
+    def load_user
       # FIXME: temporary implementation for testing
-      @user = User.first
-      @resume = @user.resume
+      @user = User.last
+    end
+
+    def load_new_resume
+      # FIXME: temporary implementation for testing
+      @resume = @user.build_resume
+      3.times { @resume.failures.build }
+    end
+
+    def load_resume_and_user
+      @resume = Resume.find(params[:id])
+      @user = @resume.user
+      @failures = @resume.failures
+    end
+
     end
 end
