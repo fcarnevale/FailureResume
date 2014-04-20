790d1cfa89b72d4441e44748f31fcfcaf4973fd7
Frank Carnevale <frank@fourward.org>
2014-04-20 13:27:53 -0400

    Add WIP
    *Temporary implementation before adding twitter auth
    *Add twitter handle slug url capability

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,21 +1,46 @@
 class ResumesController < ApplicationController
-  before_action :load_user, except: :show
-  before_action :load_new_resume, only: :new
-  before_action :load_resume_and_user, only: :show
+  before_action :load_user
+  before_action :load_resume_data, only: [:home, :show]
 
   def home
     # FIXME: temporary implementation until login gets added
-    if @user.resume
-      redirect_to resume_path(@user.resume)
+    # if current_user && current_user.resume
+    #   render 'show'
+    # elsif current_user
+    #   redirect_to new_resume_url
+    # else
+    #   redirect_to signin_url
+    # end
+
+    if @resume
+      render 'show'
     else
       redirect_to action: :new
     end
   end
 
   def new
+    # This should be something like:
+    # before_action :signed_in_restrictions
+    # def signed_in_restrictions
+    #   redirect_to edit_resume_path(current_user.resume) if signed_in_with_resume?
+    #   redirect_to signin_path, flash: { alert: "Please sign in to create a failure resume." } unless current_user
+    # end
+    # if current_user && current_user.resume
+    #   render 'show'
+    # elsif current_user
+    #   @resume = current_user.build_resume
+    #   3.times { @resume.failures.build }
+    # else
+    #   redirect_to signup_url
+    # end  
+    
+    @resume = @user.build_resume
+    3.times { @resume.failures.build }
   end
 
   def create
+    # This should be something like: @resume = current_user.build_resume(resume_params)
     @resume = @user.build_resume(resume_params)
 
     if @resume.save
@@ -26,6 +51,7 @@ class ResumesController < ApplicationController
   end
 
   def show
+    render text: 'time to implement twitter auth' unless @resume
   end
 
   private
@@ -34,16 +60,10 @@ class ResumesController < ApplicationController
       @user = User.last
     end
 
-    def load_new_resume
-      # FIXME: temporary implementation for testing
-      @resume = @user.build_resume
-      3.times { @resume.failures.build }
-    end
-
-    def load_resume_and_user
-      @resume = Resume.find(params[:id])
-      @user = @resume.user
-      @failures = @resume.failures
+    def load_resume_data
+      @user = User.find_by(twitter_handle: params[:id])
+      @resume = @user.resume if @user
+      @failures = @resume.failures if @resume
     end
 
     def resume_params
